b31f9346d7d08efed0f85cf40b4c747ccadf1ac3
Michael <meister@spacekace.com>
2017-01-19 14:37:43 -0800

    taking out tax rate

--- app/models/swell_ecom/geo_address.rb
+++ app/models/swell_ecom/geo_address.rb
@@ -2,8 +2,11 @@ module SwellEcom
 	class GeoAddress < ActiveRecord::Base
 		self.table_name = 'geo_addresses'
 
-		belongs_to :geo_state
-		belongs_to :user
+		belongs_to	:geo_state
+		belongs_to	:geo_country
+		belongs_to 	:user
+
+		enum status: { 'active' => 1, 'trash' => 2 }
 
 	end
 end
