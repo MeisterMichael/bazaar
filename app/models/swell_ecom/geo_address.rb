1f9deed2bc10cf370ba9f1be19849fe49ecdc990
Michael <meister@spacekace.com>
2017-01-19 14:34:39 -0800

    undo

--- app/models/swell_ecom/geo_address.rb
+++ app/models/swell_ecom/geo_address.rb
@@ -1,10 +1,9 @@
 module SwellEcom
 	class GeoAddress < ActiveRecord::Base
 		self.table_name = 'geo_addresses'
-		
+
 		belongs_to :geo_state
 		belongs_to :user
 
-		enum address_type: { 'billing' => 1, 'shipping' => 2 }
 	end
 end
