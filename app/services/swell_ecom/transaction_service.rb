296138eb357047ff31cc75a326c376ddd9219e85
Michael <meister@spacekace.com>
2017-06-05 20:16:21 -0700

    bug fix

--- app/services/swell_ecom/transaction_service.rb
+++ app/services/swell_ecom/transaction_service.rb
@@ -2,7 +2,7 @@ module SwellEcom
 
 	class TransactionService
 
-		def self.calculate( order )
+		def self.calculate( obj )
 
 			return self.calculate_order( obj ) if obj.is_a? Order
 			return self.calculate_cart( obj ) if obj.is_a? Cart
