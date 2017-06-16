405e0153524503fe72a20892e8a6af40639da898
Michael <meister@spacekace.com>
2017-06-16 09:02:17 -0700

    move add to cart to a page view event

--- app/controllers/swell_ecom/carts_controller.rb
+++ app/controllers/swell_ecom/carts_controller.rb
@@ -5,7 +5,16 @@ module SwellEcom
 		before_filter :get_cart
 
 		def show
-			
+
+			add_page_event_data(
+				ecommerce: {
+					add: {
+						actionField: {},
+						products: @cart.cart_items.collect{|cart_item| cart_item.item.page_event_data.merge( quantity: cart_item.quantity ) }
+					}
+				}
+			);
+
 		end
 
 		def update
