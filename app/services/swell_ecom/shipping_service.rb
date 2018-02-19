2edc4ba0c09e0a9cce4eafc95be97e2a33746fa9
Michael <meister@spacekace.com>
2018-02-19 15:30:28 -0800

    bug fix

--- app/services/swell_ecom/shipping_service.rb
+++ app/services/swell_ecom/shipping_service.rb
@@ -30,7 +30,7 @@ module SwellEcom
 			service_name = args[:service_name]
 			rates = find_order_rates( order ).sort_by{ |rate| rate[:price] }
 
-			rate = rates.select{ |rate| rate.service_name == service_name }.first if service_name.present?
+			rate = rates.select{ |rate| rate[:service_name] == service_name }.first if service_name.present?
 			rate ||= rates.first
 
 			order.order_items.new( item: nil, price: rate[:price], subtotal: rate[:price], title: 'Shipping', order_item_type: 'shipping', tax_code: '11000', properties: { 'service_name' => rate[:name], 'carrier' => rate[:carrier] } ) if rate.present?
