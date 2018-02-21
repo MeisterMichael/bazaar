26dbbc968e13891882fe67e208f39f5784d8cc02
Michael <meister@spacekace.com>
2018-02-21 13:02:06 -0800

    add relation between subscriptions and product/variation

---
+++ spec/dummy/db/migrate/20180221114900_swell_ecom_subscription_customization_migration.rb
@@ -0,0 +1,13 @@
+class SwellEcomSubscriptionCustomizationMigration < ActiveRecord::Migration[5.1]
+	def change
+
+		add_column :subscriptions, :billing_interval_value, :integer, default: 1
+		add_column :subscriptions, :billing_interval_unit, :string, default: "months"
+		add_column :subscriptions, :trial_price, :integer
+		add_column :subscriptions, :price, :integer
+
+		add_column :subscription_plans, :item_id, :integer, default: nil
+		add_column :subscription_plans, :item_type, :string, default: nil
+
+	end
+end
