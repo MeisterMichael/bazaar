ac9f975a8c94fde09acced347334ac9764271cc9
Michael <meister@spacekace.com>
2018-02-28 16:15:49 -0800

    add bill to and ship to

--- lib/tasks/install_files/swell_ecom_cart_email_migration.rb
+++ lib/tasks/install_files/swell_ecom_cart_email_migration.rb
@@ -5,5 +5,8 @@ class SwellEcomCartEmailMigration < ActiveRecord::Migration[5.1]
 		add_column :products, :cart_description, :text, default: nil
 		add_column :subscription_plans, :cart_description, :text, default: nil
 
+		add_column :geo_country, :bill_to, :bool, default: true
+		add_column :geo_country, :ship_to, :bool, default: true
+
 	end
 end
