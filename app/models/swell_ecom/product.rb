de53758467482398d2a23f01b5558c9aff33179d
Michael <meister@spacekace.com>
2018-03-01 15:06:29 -0800

    now is an attribute

--- app/models/swell_ecom/product.rb
+++ app/models/swell_ecom/product.rb
@@ -110,10 +110,6 @@ module SwellEcom
 			active? && publish_at < Time.zone.now
 		end
 
-		def sku
-			"prod-#{self.slug}"
-		end
-
 		# e.g. Product.record_search( category_name: 'Shirts', text: 'live amrap' )
 		# e.g. Product.record_search( category_id: 1, text: 'live amrap' )
 		# e.g. Product.record_search( text: 'live amrap' )
