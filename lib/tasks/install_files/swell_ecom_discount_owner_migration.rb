c10981669344e2b60c7256e587e16efb9292e3b7
Michael <meister@spacekace.com>
2018-03-14 08:47:03 -0700

    add type to discount

--- lib/tasks/install_files/swell_ecom_discount_owner_migration.rb
+++ lib/tasks/install_files/swell_ecom_discount_owner_migration.rb
@@ -7,5 +7,7 @@ class SwellEcomDiscountOwnerMigration < ActiveRecord::Migration[5.1]
 		add_index :discounts, [ :user_id, :status, :start_at ]
 		add_index :discounts, [ :user_id, :status, :end_at ]
 
+		add_index :discounts, :type, :string
+		add_index :discounts, [ :type :id ]
 	end
 end
