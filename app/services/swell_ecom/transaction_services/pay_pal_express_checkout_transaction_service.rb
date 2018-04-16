d8aa75941f2c7c52e1165f9501b61a1b0a4b28ef
Michael <meister@spacekace.com>
2018-04-16 11:03:16 -0700

    log this error to new relic

--- app/services/swell_ecom/transaction_services/pay_pal_express_checkout_transaction_service.rb
+++ app/services/swell_ecom/transaction_services/pay_pal_express_checkout_transaction_service.rb
@@ -51,6 +51,7 @@ module SwellEcom
 
 					elsif payment_amount != order.total
 
+						NewRelic::Agent.notice_error( Exception.new("PayPal checkout amount does not match invoice. #{payment_amount} vs #{order.total}") ) if defined?( NewRelic )
 						puts "PayPal checkout amount does not match invoice. #{payment_amount} vs #{order.total}" if @mode == 'sandbox'
 						order.errors.add(:base, :processing_error, message: "PayPal checkout amount does not match invoice.")
 
