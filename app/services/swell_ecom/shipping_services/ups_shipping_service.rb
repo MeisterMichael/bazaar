5d659ae2a871966591ca95c58fe9c1efd80f8d6f
Michael <meister@spacekace.com>
2018-04-27 13:56:10 -0700

    ups shipping service fixes

--- app/services/swell_ecom/shipping_services/ups_shipping_service.rb
+++ app/services/swell_ecom/shipping_services/ups_shipping_service.rb
@@ -1,5 +1,5 @@
-# require 'ups'
 # https://github.com/ptrippett/ups
+# https://www.ups.com/upsdeveloperkit/requestaccesskey?loc=en_US
 # install 'ups' gem
 
 module SwellEcom
@@ -12,7 +12,7 @@ module SwellEcom
 
 				super( args )
 
-				@ship_from	= args[:ship_from]
+				@ship_from	= args[:ship_from] || args[:shipper]
 				@shipper	= args[:shipper]
 
 				@test_mode = not( Rails.env.production? )
@@ -37,42 +37,64 @@ module SwellEcom
 			protected
 			def request_address_rates( geo_address, line_items, args = {} )
 
+				default_fields = {
+					company_name: '',
+					attention_name: '',
+					phone_number: '',
+				}
+
+				args[:shipper] ||= @shipper
+				args[:ship_from] ||= @ship_from
+
+				args[:shipper] = default_fields.merge( args[:shipper] )
+				args[:ship_from] = default_fields.merge( args[:ship_from] )
+
+
 				begin
-					response = server.rates do |rate_builder|
+					response = @server.rates do |rate_builder|
+
 						rate_builder.add_access_request @api_key, @username, @password
 
-						rate_builder.add_shipper ( args[:shipper] || @shipper )
-						rate_builder.add_ship_from ( args[:ship_from] || @ship_from )
-						rate_builder.add_ship_to phone_number: geo_address.phone,
+						rate_builder.add_shipper args[:shipper]
+
+						rate_builder.add_ship_from args[:ship_from]
+
+						ship_to = default_fields.merge({
+							phone_number: geo_address.phone,
 							address_line_1: geo_address.street,
 							address_line_2: geo_address.street2,
 							city: geo_address.city,
-							state: geo_address.state_name,
+							state: geo_address.state_abbrev,
 							postal_code: geo_address.zip,
 							country: geo_address.geo_country.abbrev
+						})
+						rate_builder.add_ship_to ship_to
 
 						line_items.each do |line_item|
 							# convert from grams to KGs
 							package_weight_kgs = (line_item.package_weight * 0.001)
 
-							rate_builder.add_package weight: package_weight_kgs.to_s, unit: 'KGS'
+							# rate_builder.add_package weight: package_weight_kgs.to_s, unit: 'KGS'
+							rate_builder.add_package weight: (package_weight_kgs * 2.20462).to_s, unit: 'LBS'
 						end
 					end
 
 					if response.success?
 
 						rates = response.rated_shipments.collect do |rate|
-							{ name: rate[:service_name], code: rate[:service_code], price: (rate[:total].to_f * 100.o).to_i, carrier: 'UPS', currency: 'USD' }
+							{ name: rate[:service_name], code: rate[:service_code], price: (rate[:total].to_f * 100.0).to_i, carrier: 'UPS', currency: 'USD' }
 						end
 
 					else
 
+						geo_address.errors.add( :base, :shipping_error, message: 'Not a valid shipping destination.' )
+						NewRelic::Agent.notice_error(Exception.new(response.status_code+' > '+response.error_description)) if defined?( NewRelic )
 						rates = []
 
 					end
 
 				rescue Exception => e
-
+					raise e if Rails.env.development?
 					NewRelic::Agent.notice_error(e) if defined?( NewRelic )
 					rates = []
 				end
