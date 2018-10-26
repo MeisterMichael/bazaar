89469d1cc32fec29117c42f13b6f455a0500fa12
Michael Ferguson <meister@spacekace.com>
2018-10-26 09:13:04 -0700

    tweak

--- app/controllers/bazaar/orders_controller.rb
+++ app/controllers/bazaar/orders_controller.rb
@@ -5,18 +5,19 @@ module Bazaar
 		def thank_you
 			@order = Order.find_by( code: params[:id] )
 
-			if current_user.present?
-				raise ActionController::RoutingError.new( 'Not Found' ) if current_user != @order.user
-			else
-				begin
+
+			begin
+				if current_user.present?
+					raise ActionController::RoutingError.new( 'Not Found' ) if current_user != @order.user
+				else
 					verified_message = Rails.application.message_verifier('order.id').verify(params[:d])
 					raise ActionController::RoutingError.new( 'Not Found' ) unless verified_message[:id] == @order.id && verified_message[:code] == params[:id] && verified_message[:expiration].to_s == params[:t]
 					raise ActionController::RoutingError.new( 'Not Found' ) if Time.now.to_i > params[:t].to_i
-				rescue Exception => e
-					set_flash 'Login to view your orders'
-					redirect_to '/login'
-					return false
 				end
+			rescue Exception => e
+				set_flash 'Your session has expired, please login to continue'
+				redirect_to '/login'
+				return false
 			end
 
 			set_page_meta(
