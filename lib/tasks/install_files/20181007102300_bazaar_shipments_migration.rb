2670eb2cf174e680db133e5a784bb83e312c49d6
Michael Ferguson <meister@spacekace.com>
2018-11-07 14:37:20 -0800

    tentative schema

---
+++ lib/tasks/install_files/20181007102300_bazaar_shipments_migration.rb
@@ -0,0 +1,92 @@
+class BazaarShipmentsMigration < ActiveRecord::Migration[5.1]
+	def change
+
+		create_table :bazaar_shipments do |t|
+			t.integer			:status, default: 0
+			t.string			:carrier_status, default: nil
+			t.text				:notes, default: nil
+			t.belongs_to	:order, default: nil
+
+			# The Who - is preparing and delivering the shipment
+			t.string			:fulfilled_by, default: nil
+
+			# The What - size and weight of the shipment
+			t.float				:length
+			t.float				:width
+			t.float				:height
+			t.integer			:shape, default: 1
+			t.float				:weight
+			t.integer			:cost
+
+			# The Where - it's going, coming from and where it is along the way
+			t.belongs_to	:destination_address
+			t.belongs_to	:source_address
+			t.belongs_to	:warehouse
+			t.string			:tracking_code, default: nil
+			t.string			:tracking_url, default: nil
+
+			# The When
+			t.datetime		:estimated_delivered_at
+			t.datetime		:shipped_at
+			t.datetime		:delivered_at
+			t.datetime		:returned_at
+
+			# The How - the shipment is being sent
+			t.string			:carrier, default: nil
+			t.string			:carrier_service_level, default: nil
+
+			t.hstore			:properties
+			t.timestamps
+		end
+
+		create_table :bazaar_shipment_logs do |t|
+			t.belongs_to	:shipment
+			t.string			:carrier_status, default: nil
+			t.string			:subject
+			t.text				:details
+			t.hstore			:properties
+			t.timestamps
+		end
+
+		create_table :bazaar_shipment_skus do |t|
+			t.belongs_to	:shipment
+			t.belongs_to	:sku
+			t.integer			:quantity
+			t.timestamps
+		end
+
+		create_table :bazaar_skus do |t|
+			t.string			:name
+			t.text				:description
+			t.string			:code
+			t.float				:length
+			t.float				:width
+			t.float				:height
+			t.integer			:shape, default: 1
+			t.float				:weight
+			t.timestamps
+		end
+
+		create_table :bazaar_warehouses do |t|
+			t.string			:name
+			t.belongs_to	:geo_address
+			t.integer			:status, default: 1
+			t.timestamps
+		end
+
+		create_table :bazaar_warehouse_skus do |t|
+			t.belongs_to	:warehouse
+			t.belongs_to	:sku
+			t.integer			:quantity
+			t.datetime		:quantity_updated_at
+			t.timestamps
+		end
+
+
+		add_column :bazaar_products, :sku_id, :integer, default: nil
+		add_column :bazaar_subscription_plans, :trial_sku_id, :integer, default: nil
+		add_column :bazaar_subscription_plans, :sku_id, :integer, default: nil
+
+
+	end
+end
