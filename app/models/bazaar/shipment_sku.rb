0c02d7e988905c92dab587ba9eaa8d89b2069897
Michael Ferguson <meister@spacekace.com>
2018-11-26 15:07:56 -0800

    find wahrehouse sku if warehouse is present in shipment

--- app/models/bazaar/shipment_sku.rb
+++ app/models/bazaar/shipment_sku.rb
@@ -2,5 +2,16 @@ module Bazaar
 	class ShipmentSku < ApplicationRecord
 		belongs_to	:shipment
 		belongs_to	:sku
+
+		def warehouse_sku
+			warehouse = shipment_sku.shipment.warehouse
+
+			if warehouse.present?
+				self.sku.warehouse_skus.where( warehouse: warehouse ).first
+			else
+				nil
+			end
+		end
+
 	end
 end
