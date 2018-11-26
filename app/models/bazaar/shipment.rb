68957841cb6510f6c7258aa7d8ca82ad8e54dfc4
Michael Ferguson <meister@spacekace.com>
2018-11-26 15:08:13 -0800

    validate that warehouse supports all shipment skus.

--- app/models/bazaar/shipment.rb
+++ app/models/bazaar/shipment.rb
@@ -13,8 +13,20 @@ module Bazaar
 		enum status: { 'canceled' => -1, 'pending' => 0, 'picking' => 100, 'packed' => 200, 'shipped' => 300, 'delivered' => 400, 'returned' => 500 }
 		enum package_shape: { 'no_shape' => 0, 'letter' => 1, 'box' => 2, 'cylinder' => 3 }
 
+		validate :validate_warehouse_skus
+
 		money_attributes :cost
 
+		protected
+		def validate_warehouse_skus
+			if self.warehouse.present?
+				self.shipment_skus.each do |shipment_sku|
+					warehouse_sku = shipment_sku.warehouse_sku
+
+					self.errors.add :base, "Warehouse '#{warehouse_sku.warehouse.name}' does not support the sku #{shipment_sku.sku.code}") unless warehouse_sku.present? && warehouse_sku.code.present?
+				end
+			end
+		end
 
 	end
 end
