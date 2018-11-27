4c6001d5e415cb85e2ce9e7f570f5e4d0d5daa85
Michael Ferguson <meister@spacekace.com>
2018-11-27 14:56:32 -0800

    fix

--- app/models/bazaar/shipment.rb
+++ app/models/bazaar/shipment.rb
@@ -23,7 +23,7 @@ module Bazaar
 				self.shipment_skus.each do |shipment_sku|
 					warehouse_sku = shipment_sku.warehouse_sku
 
-					self.errors.add :base, "Warehouse '#{warehouse_sku.warehouse.name}' does not support the sku #{shipment_sku.sku.code}") unless warehouse_sku.present? && warehouse_sku.code.present?
+					self.errors.add( :base, "Warehouse '#{warehouse_sku.warehouse.name}' does not support the sku #{shipment_sku.sku.code}") unless warehouse_sku.present? && warehouse_sku.code.present?
 				end
 			end
 		end
