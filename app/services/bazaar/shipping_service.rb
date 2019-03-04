a9117bec1ae1c1e9aaf7eabca5dc55a6b7d071bb
Michael Ferguson <meister@spacekace.com>
2019-03-04 15:52:26 -0800

    remove not shipment yet

--- app/services/bazaar/shipping_service.rb
+++ app/services/bazaar/shipping_service.rb
@@ -87,11 +87,6 @@ module Bazaar
 			if rate.present?
 				shipping_order_item = order.order_items.new( item: rate[:carrier_service], price: rate[:price], subtotal: rate[:price], title: (rate[:label] || rate[:name]), order_item_type: 'shipping', tax_code: '11000', properties: { 'name' => rate[:name], 'code' => rate[:code], 'carrier' => rate[:carrier] } )
 				order.shipping = rate[:price]
-
-				shipment.carrier_service_level = shipping_order_item.item.service_name if shipping_order_item.item.respond_to?(:service_name)
-				shipment.price = rate[:price]
-				shipment.cost = rate[:cost]
-
 			else
 				order.shipping = 0
 			end
