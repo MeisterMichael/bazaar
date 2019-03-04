ed1dea8390c5a52c37d4f08cdae28f6a08f72a03
Michael Ferguson <meister@spacekace.com>
2019-03-04 11:58:20 -0800

    subscriptions

--- app/services/bazaar/order_service.rb
+++ app/services/bazaar/order_service.rb
@@ -242,6 +242,9 @@ module Bazaar
 
 				offer_price = offer.offer_prices.active.for_interval( 1 ).first.price
 				new_order_offer = order_item.order.order_offers.new( offer: offer, tax_code: offer.tax_code, title: offer.title, quantity: order_item.quantity, price: offer_price, subtotal: offer_price * order_item.quantity )
+				new_order_offer.subscription = order_item.subscription
+				new_order_offer.subscription = order_item.item if order_item.item.is_a? Bazaar::Subscription
+				new_order_offer.subscription_interval = Bazaar::OrderOffer.joins(:order).merge(Bazaar::Order.positive_status).where( subscription: new_order_offer.subscription ).maximum(:subscription_interval).to_i + 1 if new_order_offer.subscription
 
 				new_order_offer.offer.offer_skus.active.for_interval( new_order_offer.subscription_interval ).each do |offer_sku|
 					order_sku = order_item.order.order_skus.to_a.find{ |order_sku| order_sku.sku == offer_sku.sku }
