42a2a12b9047cf57395f820cedf70ebc3d82b125
Michael Ferguson <meister@spacekace.com>
2019-03-20 11:27:01 -0700

    cancel fulfilllment on refund optionally

--- app/controllers/bazaar/order_admin_controller.rb
+++ app/controllers/bazaar/order_admin_controller.rb
@@ -159,8 +159,10 @@ module Bazaar
 
 				@order.refunded!
 
-				# cancel fulfillment if a full refund and not already fulfilled/delivered
-				@order.fulfillment_canceled! if @order.transactions.approved.negative.sum(:amount) >= @order.transactions.approved.positive.sum(:amount) && not( @order.fulfilled? || @order.delivered? )
+				if params[:cancel_fullfillment]
+					# cancel fulfillment if a full refund and not already fulfilled/delivered
+					@order.fulfillment_canceled! if @order.transactions.approved.negative.sum(:amount) >= @order.transactions.approved.positive.sum(:amount) && not( @order.fulfilled? || @order.delivered? )
+				end
 
 				# OrderMailer.refund( @transaction ).deliver_now # send emails on a cron
 				set_flash "Refund successful", :success
