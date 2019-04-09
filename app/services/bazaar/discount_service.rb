1eb6205b7a8565fec69541c5782977091d4e33b6
Michael Ferguson <meister@spacekace.com>
2019-04-09 10:19:22 -0700

    discount max min limit fix
    need to account for failed and other persisted orders and not include them against limits

--- app/services/bazaar/discount_service.rb
+++ app/services/bazaar/discount_service.rb
@@ -27,14 +27,17 @@ module Bazaar
 			shipping_order_items	= order.order_items.select{ |order_item| order_item.shipping? }
 			tax_order_items			= order.order_items.select{ |order_item| order_item.tax? }
 
+			all_not_self_positive_status_orders = Order.positive_status.where.not( id: order.id )
+			all_discount_order_items = OrderItem.where( item: discount ).joins(:order)
+
 			error_messages = []
 			error_messages << 'Invalid discount' if not( discount.active? ) || not( discount.in_progress? )
 			error_messages << 'Unsupported discount type' if discount.selected_users?
 			error_messages << 'The discount does not meet the minimum purchase requirement' if discount.minimum_prod_subtotal != 0 && discount.minimum_prod_subtotal > prod_order_items.sum{ |order_item| order_item.subtotal }
 			error_messages << 'The discount does not meet the minimum shipping requirement' if discount.minimum_shipping_subtotal != 0 && discount.minimum_shipping_subtotal > shipping_order_items.sum{ |order_item| order_item.subtotal }
 			error_messages << 'The discount does not meet the minimum tax requirement' if discount.minimum_tax_subtotal != 0 && discount.minimum_tax_subtotal > tax_order_items.sum{ |order_item| order_item.subtotal }
-			error_messages << 'You have exceeded the limit of uses for the selected discount' if discount.limit_per_customer.present? && order.user.present? && OrderItem.where( item: discount ).joins(:order).merge( Order.where( user: order.user ).positive_status ).count >= discount.limit_per_customer
-			error_messages << 'The selected discount\'s usage limit has been exhausted' if discount.limit_global.present? && OrderItem.where( item: discount ).joins(:order).merge( Order.positive_status ).count >= discount.limit_global
+			error_messages << 'You have exceeded the limit of uses for the selected discount' if discount.limit_per_customer.present? && order.user.present? && all_discount_order_items.merge( all_not_self_positive_status_orders.where( user: order.user ) ).count >= discount.limit_per_customer
+			error_messages << 'The selected discount\'s usage limit has been exhausted' if discount.limit_global.present? && all_discount_order_items.merge( all_not_self_positive_status_orders ).count >= discount.limit_global
 
 			error_messages
 		end
@@ -56,8 +59,21 @@ module Bazaar
 
 				order_items = order_items.select{ |order_item| not( order_item.discount? ) }
 				order_items = order_items.select{ |order_item| order_item.order_item_type == discount_item.order_item_type } unless discount_item.all_order_item_types?
-				order_items = order_items.select{ |order_item| order_item.item.is_a?( Subscription ) && order_item.item.orders.not_declined.count >= discount_item.minimum_orders } if discount_item.minimum_orders > 0
-				order_items = order_items.select{ |order_item| not( order_item.item.is_a?( Subscription ) ) || OrderItem.discount.joins(:order).merge( order_item.item.orders.not_declined ).where( item_id: discount_item.discount, item_type: discount_item.discount.class.base_class.name ).count < discount_item.maximum_orders } unless discount_item.maximum_orders.to_i <= 1
+				order_items = order_items.select do |order_item|
+					keep = true
+
+					if ( subscription = order_item.item ).is_a?( Subscription )
+						this_discount_order_items = OrderItem.discount.joins(:order).merge( subscription.orders.not_declined.where.not( id: order.id ) ).where( item: discount_item.discount )
+
+						keep = false if discount_item.minimum_orders.to_i > 0 || discount_item.maximum_orders.to_i > 1
+						keep = keep || (this_discount_order_items.count >= discount_item.minimum_orders) if discount_item.minimum_orders.to_i > 0
+						keep = keep || (this_discount_order_items.count < discount_item.maximum_orders) if discount_item.maximum_orders.to_i > 1
+					elsif ( plan = order_item.item ).is_a?( SubscriptionPlan ) && discount_item.minimum_orders.to_i > 0
+						keep = false
+					end
+
+					keep
+				end
 
 				if discount_item.applies_to.is_a?( Bazaar::Collection )
 
