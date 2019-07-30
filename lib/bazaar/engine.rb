6d74f41a7b90e9dd3cc7fba62307ffa079033286
Michael Ferguson <meister@spacekace.com>
2019-07-30 15:30:28 -0700

    shipment_code_prefix/shipment_code_postfix

--- lib/bazaar/engine.rb
+++ lib/bazaar/engine.rb
@@ -58,6 +58,9 @@ module Bazaar
 		mattr_accessor :order_code_prefix
 		mattr_accessor :order_code_postfix
 
+		mattr_accessor :shipment_code_prefix
+		mattr_accessor :shipment_code_postfix
+
 		mattr_accessor :subscription_code_prefix
 		mattr_accessor :subscription_code_postfix
 
