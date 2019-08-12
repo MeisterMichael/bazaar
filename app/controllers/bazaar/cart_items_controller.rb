25278b7d34612494590701abd7380d779a83e483
Michael Ferguson <meister@spacekace.com>
2019-08-12 14:22:28 -0700

    respond to cart_items_controller create

--- app/controllers/bazaar/cart_items_controller.rb
+++ app/controllers/bazaar/cart_items_controller.rb
@@ -59,10 +59,17 @@ module Bazaar
 
 			#set_flash "<div class='row'><div class='col-xs-3 col-sm-2 col-lg-1'><img src='#{@item.avatar}' class='img img-responsive' /></div> <div class='col-xs-9 col-sm-10 col-lg-11'>#{count}#{@item.title} Added to your <a href='/cart'>Cart</a>. <br> <a href='/checkout'>Checkout</a>, or <a href='#' data-dismiss='alert'> Keep Shopping</a>.</div></div>"
 
-			if params[:buy_now]
-				redirect_to bazaar.checkout_index_path( buy_now: 1 )
-			else
-				redirect_to '/cart'
+
+			respond_to do |format|
+				format.json {
+				}
+				format.html {
+					if params[:buy_now]
+						redirect_to bazaar.checkout_index_path( buy_now: 1 )
+					else
+						redirect_to '/cart'
+					end
+				}
 			end
 		end
 
