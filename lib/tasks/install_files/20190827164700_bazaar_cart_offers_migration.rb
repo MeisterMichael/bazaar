de4af5fb38618e948c277fea3b7fedef95eba3a8
Michael Ferguson <meister@spacekace.com>
2019-09-18 11:39:43 -0700

    add product id to offer

--- lib/tasks/install_files/20190827164700_bazaar_cart_offers_migration.rb
+++ lib/tasks/install_files/20190827164700_bazaar_cart_offers_migration.rb
@@ -4,5 +4,9 @@ class BazaarCartOffersMigration < ActiveRecord::Migration[5.1]
 		rename_table :bazaar_cart_items, :bazaar_cart_offers
 		add_column :bazaar_cart_offers, :offer_id, :bigint, default: nil
 
+		add_column :bazaar_offers, :product_id, :bigint, default: nil
+		add_index :bazaar_offers, [:product_id,:status,:created_at]
+
+
 	end
 end
