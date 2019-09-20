b45ea85f2d4a6dc411f7f88ebc54b38766062f78
Michael Ferguson <meister@spacekace.com>
2019-09-20 14:41:20 -0700

    add period type to offer schedule
    for identifying trial periods

--- lib/tasks/install_files/20190827164700_bazaar_cart_offers_migration.rb
+++ lib/tasks/install_files/20190827164700_bazaar_cart_offers_migration.rb
@@ -4,8 +4,9 @@ class BazaarCartOffersMigration < ActiveRecord::Migration[5.1]
 		rename_table :bazaar_cart_items, :bazaar_cart_offers
 		add_column :bazaar_cart_offers, :offer_id, :bigint, default: nil
 
-		add_column :bazaar_offers, :product_id, :bigint, default: nil
-		add_index :bazaar_offers, [:product_id,:status,:created_at]
+		add_column :bazaar_order_offers, :properties, :hstore, default: {}
+
+		add_column :bazaar_offer_schedules, :period_type, :string, default: nil
 
 
 	end
