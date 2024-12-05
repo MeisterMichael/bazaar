40a1f3b0a58f59c35a56ab11fb1e649c37f580c5
Michael Ferguson <meister@spacekace.com>
2024-12-05 14:04:24 -0800

    still need to test on working creds but otherwise seems to be working

--- app/services/bazaar/transaction_services/authorize_dot_net_transaction_service.rb
+++ app/services/bazaar/transaction_services/authorize_dot_net_transaction_service.rb
@@ -31,12 +31,12 @@ module Bazaar
 			end
 
 			def capture_payment_method( order, args = {} )
-				credit_card_info = args[:credit_card]
+				payment_details = extract_payment_details( args )
 
 				self.calculate( order )
 				return false if order.nested_errors.present?
 
-				profiles = get_order_customer_profile( order, credit_card: credit_card_info )
+				profiles = get_order_customer_profile( order, payment_details )
 				return false if profiles == false
 
 				order.payment_status = 'payment_method_captured'
@@ -72,12 +72,12 @@ module Bazaar
 			end
 
 			def process( order, args = {} )
-				credit_card_info = args[:credit_card]
+				payment_details = extract_payment_details( args )
 
 				self.calculate( order )
 				return false if order.nested_errors.present?
 
-				profiles = get_order_customer_profile( order, credit_card: credit_card_info )
+				profiles = get_order_customer_profile( order, payment_details )
 				return false if profiles == false
 
 				order.provider = @provider_name
@@ -103,16 +103,10 @@ module Bazaar
 
 				transaction_properties = {}
 
-				if credit_card_info.present? && credit_card_info[:card_number].present?
-					credit_card_dector = CreditCardValidations::Detector.new( credit_card_info[:card_number] )
+				if payment_details.present?
 
-					new_properties = {
-						'credit_card_ending_in' => credit_card_dector.number[-4,4],
-						'credit_card_brand' => credit_card_dector.brand,
-					}
-
-					order.properties = order.properties.merge(new_properties)
-					transaction_properties = new_properties
+					order.properties = order.properties.merge(payment_details[:meta_data])
+					transaction_properties = payment_details[:meta_data]
 
 				elsif ( first_profile_transaction = Bazaar::Transaction.where( provider: @provider_name, customer_profile_reference: profiles[:customer_profile_reference], customer_payment_profile_reference: profiles[:customer_payment_profile_reference] ).where.not(credit_card_ending_in: nil).first ).present?
 
@@ -137,7 +131,7 @@ module Bazaar
 
 				transaction.save
 
-				process_transaction( transaction )
+				process_transaction( transaction, payment_details )
 
 				# process response
 				if transaction.approved?
@@ -174,12 +168,11 @@ module Bazaar
 				return false
 			end
 
-			def process_transaction( transaction, args = {} )
+			def process_transaction( transaction, payment_details, args = {} )
 
-				credit_card_info = args[:credit_card]
 				if credit_card_info.present? && transaction.parent_obj.present?
 
-					profiles = get_order_customer_profile( transaction.parent_obj, credit_card: credit_card_info )
+					profiles = get_order_customer_profile( transaction.parent_obj, payment_details )
 					if profiles == false
 						transaction.status = 'declined'
 						transaction.message = "Unable to create customer profile"
@@ -321,8 +314,8 @@ module Bazaar
 				transaction.customer_profile_reference ||= charge_transaction.customer_profile_reference
 				transaction.customer_payment_profile_reference ||= charge_transaction.customer_payment_profile_reference
 
-				transaction.properties						= charge_transaction.properties.merge( transaction.properties ) if charge_transaction
-				transaction.credit_card_ending_in	= charge_transaction.credit_card_ending_in if charge_transaction.respond_to?(:credit_card_ending_in)
+				transaction.properties					= charge_transaction.properties.merge( transaction.properties ) if charge_transaction
+				transaction.credit_card_ending_in		= charge_transaction.credit_card_ending_in if charge_transaction.respond_to?(:credit_card_ending_in)
 				transaction.credit_card_brand			= charge_transaction.credit_card_brand if charge_transaction.respond_to?(:credit_card_brand)
 				transaction.billing_address				= charge_transaction.billing_address if charge_transaction.respond_to?(:billing_address)
 
@@ -421,24 +414,20 @@ module Bazaar
 			end
 
 			def update_subscription_payment_profile( subscription, args = {} )
-				payment_profile = request_payment_profile( subscription.user, subscription.billing_address, args[:credit_card], errors: subscription.errors, ip: subscription.order.try(:ip) )
+
+				payment_details = extract_payment_details( args )
+
+				payment_profile = request_payment_profile( subscription.user, subscription.billing_address, payment_details, errors: subscription.errors, ip: subscription.order.try(:ip) )
 
 				return false unless payment_profile
-
-				credit_card_dector = CreditCardValidations::Detector.new( args[:credit_card][:card_number] )
-
-				new_properties = {
-					'credit_card_ending_in' => credit_card_dector.number[-4,4],
-					'credit_card_brand' => credit_card_dector.brand,
-				}
 
 				subscription.provider = @provider_name
 				subscription.transaction_provider = self.transaction_provider
 				subscription.merchant_identification = self.merchant_identification
 				subscription.provider_customer_profile_reference = payment_profile[:customer_profile_reference]
 				subscription.provider_customer_payment_profile_reference = payment_profile[:customer_payment_profile_reference]
-				subscription.properties = subscription.properties.merge( new_properties )
-				subscription.payment_profile_expires_at	= Bazaar::TransactionService.parse_credit_card_expiry( args[:credit_card][:expiration] ) if subscription.respond_to?(:payment_profile_expires_at)
+				subscription.properties = subscription.properties.merge( payment_details[:meta_data] )
+				subscription.payment_profile_expires_at	= payment_details[:expires_at] if subscription.respond_to?(:payment_profile_expires_at)
 
 				subscription.save
 
@@ -446,18 +435,89 @@ module Bazaar
 
 			protected
 
-			def get_order_customer_profile( order, args = {} )
+			def extract_payment_details( args = {} )
+				payment_details = { error: 'invalid payment details' }
 
 				if args[:credit_card].present?
 
-					payment_profile = request_payment_profile( order.user, order.billing_address, args[:credit_card], email: order.email, errors: order.errors, ip: order.ip )
+					card_number = args[:credit_card][:card_number]
+					expiration_str = args[:credit_card][:expiration]
+					card_code = args[:credit_card][:card_code]
+
+					credit_card_dector = CreditCardValidations::Detector.new( card_number )
+
+					expires_at = Bazaar::TransactionService.parse_credit_card_expiry( expiration_str )
+
+					meta_data = {
+						'credit_card_ending_in' => credit_card_dector.number[-4,4],
+						'credit_card_brand' => credit_card_dector.brand,
+					}
+
+					details = {
+						card_number: card_number,
+						expiration: expiration_str,
+						card_code: card_code,
+					}
+
+					payment_details = {
+						type: 'credit_card',
+						meta_data: meta_data,
+						details: details,
+						expires_at: expires_at,
+					}
+
+					if expires_at < Time.now
+						payment_details[:error] = 'Credit Card is Expired'
+					elsif not( credit_card_dector.valid? )
+						payment_details[:error] = 'Invalid Credit Card Number'
+					end
+
+				elsif args[:google_pay].present?
+
+					payment_data = JSON.parse(args.dig(:google_pay,:payment_data) || '{}', :symbolize_names => true )
+					payment_token = payment_data.dig(:paymentMethodData,:tokenizationData,:token)
+					payment_token_type = payment_data.dig(:paymentMethodData,:tokenizationData,:type)
+
+					meta_data = {
+						'credit_card_ending_in' => payment_data.dig(:paymentMethodData,:info,:cardDetails),
+						'credit_card_brand' => payment_data.dig(:paymentMethodData,:info,:cardNetwork),
+					}
+
+					details = {
+						token: payment_token,
+						token_type: payment_token_type,
+						results: args[:google_pay],
+					}
+
+					payment_details = {
+						type: 'google_pay',
+						meta_data: meta_data,
+						details: details,
+					}
+
+					if payment_token.blank?
+						payment_details[:error] = 'Invalid Google Pay Token'
+					end
+				else
+
+					raise Exception.new("Unable to extract payment details")
+				end
+
+				payment_details
+			end
+
+			def get_order_customer_profile( order, payment_details, args = {} )
+
+				if payment_details[:error].blank?
+
+					payment_profile = request_payment_profile( order.user, order.billing_address, payment_details, email: order.email, errors: order.errors, ip: order.ip )
 
 					return payment_profile if payment_profile && order.nested_errors.blank?
 
 				else
 					return { customer_profile_reference: order.provider_customer_profile_reference, customer_payment_profile_reference: order.provider_customer_payment_profile_reference } if order.provider_customer_profile_reference.present?
 
-					raise Exception.new( 'cannot create payment profile without credit card info' )
+					raise Exception.new( 'cannot create payment profile without valid payment info' )
 
 				end
 
@@ -465,7 +525,7 @@ module Bazaar
 			end
 
 
-			def request_payment_profile( user, billing_address, credit_card, args={} )
+			def request_payment_profile( user, billing_address, payment_details, args={} )
 				anet_transaction = AuthorizeNet::API::Transaction.new(@api_login, @api_key, :gateway => @gateway )
 				errors = args[:errors]
 
@@ -490,35 +550,49 @@ module Bazaar
 				anet_billing_address.country			= billing_address.geo_country.name
 				anet_billing_address.phoneNumber	= billing_address.phone
 
-				# VALIDATE Credit card number
-				credit_card_dector = CreditCardValidations::Detector.new(credit_card[:card_number])
-				unless credit_card_dector.valid?
-					errors.add( :base, 'Invalid Credit Card Number' ) if errors
+				# report any errors with the payment details
+				if payment_details[:error].present?
+					errors.add( :base, payment_details[:error] ) if errors
 					return false
 				end
 
-				# VALIDATE Credit card expirey
-				expiration_time = Bazaar::TransactionService.parse_credit_card_expiry( credit_card[:expiration] )
-				if expiration_time.nil?
-					errors.add( :base, 'Credit Card Expired is required') if errors
-					return false
-				elsif expiration_time.end_of_month < Time.now.end_of_month
-					errors.add( :base, 'Credit Card has Expired') if errors
-					return false
+
+				if payment_details[:type] == 'credit_card'
+
+					# VALIDATE Credit card expirey
+					if payment_details[:expires_at].nil?
+						errors.add( :base, 'Credit Card Expired is required') if errors
+						return false
+					elsif payment_details[:expires_at].end_of_month < Time.now.end_of_month
+						errors.add( :base, 'Credit Card has Expired') if errors
+						return false
+					end
+
+					formatted_expiration = payment_details[:details][:expiration].gsub( /\/\s*\d\d(\d\d)/, '/\\1' ).gsub(/\s*\/\s*/,'')
+					formatted_number = payment_details[:details][:card_number].gsub(/\s/,'')
+					
+					anet_payment = AuthorizeNet::API::PaymentType.new(AuthorizeNet::API::CreditCardType.new)
+					anet_payment.creditCard.cardNumber = formatted_number
+					anet_payment.creditCard.expirationDate = formatted_expiration
+					anet_payment.creditCard.cardCode = payment_details[:details][:card_code]
+
+					anet_payment_profile = AuthorizeNet::API::CustomerPaymentProfileType.new
+					anet_payment_profile.payment	= anet_payment
+					anet_payment_profile.billTo		= anet_billing_address
+
+				elsif payment_details[:type] == 'google_pay'
+
+					anet_payment = AuthorizeNet::API::PaymentType.new(AuthorizeNet::API::OpaqueDataType.new)
+					anet_payment.creditCard = nil
+					anet_payment.opaqueData = AuthorizeNet::API::OpaqueDataType.new
+					anet_payment.opaqueData.dataDescriptor = 'COMMON.GOOGLE.INAPP.PAYMENT'
+					# anet_payment.opaqueData.dataValue = payment_details[:details][:token]
+					anet_payment.opaqueData.dataValue = Base64.strict_encode64(payment_details[:details][:token])
+
+					anet_payment_profile = AuthorizeNet::API::CustomerPaymentProfileType.new
+					anet_payment_profile.payment	= anet_payment
+
 				end
-
-				formatted_expiration = credit_card[:expiration].gsub( /\/\s*\d\d(\d\d)/, '/\\1' ).gsub(/\s*\/\s*/,'')
-				formatted_number = credit_card[:card_number].gsub(/\s/,'')
-
-				anet_credit_card = AuthorizeNet::API::PaymentType.new(AuthorizeNet::API::CreditCardType.new)
-				anet_credit_card.creditCard.cardNumber = formatted_number
-				anet_credit_card.creditCard.expirationDate = formatted_expiration
-				anet_credit_card.creditCard.cardCode = credit_card[:card_code]
-
-				anet_payment_profile = AuthorizeNet::API::CustomerPaymentProfileType.new
-				anet_payment_profile.payment	= anet_credit_card
-				anet_payment_profile.billTo		= anet_billing_address
-				# anet_payment_profile.defaultPaymentProfile = true
 
 				# anet_customer_profile = AuthorizeNet::API::CustomerProfile.new(
 				# 	:email			=> args[:email] || user.try(:email),
