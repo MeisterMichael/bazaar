35acccfaea4f79f836a8c9ddb429947fa63ab9a7
Michael Ferguson <meister@spacekace.com>
2025-05-02 15:46:08 -0700

    deliver cancel subscription mail

--- app/mailers/bazaar/subscription_mailer.rb
+++ app/mailers/bazaar/subscription_mailer.rb
@@ -1,6 +1,12 @@
 module Bazaar
 	class SubscriptionMailer < ActionMailer::Base
 
+		def cancel_subscription( subscription )
+			@subscription = subscription
+			subject = "Your subscription was canceled"
+			mail to: @subscription.user.email, from: Bazaar.order_email_from, subject: subject
+		end
+
 		def payment_profile_expiration_reminder( subscription )
 			@subscription = subscription
 			subject = "Your payment information is about to expire"
