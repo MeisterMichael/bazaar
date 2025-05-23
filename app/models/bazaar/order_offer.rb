ab7d988849c768ad737f2efc31df9d23aa54f302
Michael Ferguson <meister@spacekace.com>
2025-05-23 15:11:41 -0700

    order_offer subscription_offer

--- app/models/bazaar/order_offer.rb
+++ app/models/bazaar/order_offer.rb
@@ -4,12 +4,15 @@ module Bazaar
 
 		belongs_to :offer
 		belongs_to :order
+		belongs_to :subscription_offer, required: false, validate: true
+		# belongs_to :subscription_period, required: false, validate: true
 		belongs_to :subscription, required: false, validate: true
 		belongs_to :source_obj, required: false, polymorphic: true
 		belongs_to :upsell_offer, required: false
 		belongs_to :upsell, required: false
 
 		has_many :order_offer_discounts
+		# has_many :subscription_logs
 
 		money_attributes :subtotal, :price, :tax
 
