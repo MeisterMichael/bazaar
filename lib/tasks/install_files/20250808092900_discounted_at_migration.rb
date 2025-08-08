b66d6e6893e5cd179ab9aa43c749f66d3dba1a47
Michael Ferguson <meister@spacekace.com>
2025-08-08 10:15:35 -0700

    tweak

--- lib/tasks/install_files/20250808092900_discounted_at_migration.rb
+++ lib/tasks/install_files/20250808092900_discounted_at_migration.rb
@@ -3,7 +3,9 @@ class DiscountedAtMigration < ActiveRecord::Migration[7.1]
 
 		change_table :bazaar_carts do |t|
 			t.timestamp	:discounted_at
+			t.timestamp	:discount_expired_at
 			t.index [:discounted_at,:discount_id]
+			t.index [:discount_expired_at,:discount_id]
 		end
 
 	end
