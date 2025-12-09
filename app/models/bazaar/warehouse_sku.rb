ff3530c7d6b64fa0ce72276a68565f7a2736c469
Michael Ferguson <meister@spacekace.com>
2025-12-09 08:54:50 -0800

    validate warehouse_skus split

--- app/models/bazaar/warehouse_sku.rb
+++ app/models/bazaar/warehouse_sku.rb
@@ -8,6 +8,8 @@ module Bazaar
 		enum country_restriction_type: { 'countries_blacklist' => -1, 'countries_unrestricted' => 0, 'countries_whitelist' => 1 }
 		enum state_restriction_type: { 'states_blacklist' => -1, 'states_unrestricted' => 0, 'states_whitelist' => 1 }
 
+		validates :split, uniqueness: { scope: :sku_id, message: "exists for this SKU with another warehouse" }, if: :split
+
 		def code
 			self.warehouse_code || self.sku.try(:code)
 		end
